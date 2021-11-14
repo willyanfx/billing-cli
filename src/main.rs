0dc2766d2b507259f5219c4cd0502527d9dd0141
willyanfx <willyan.fx@gmail.com>
2021-11-13 18:16:48 -0800

    stage 1

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,27 @@
-// Project 1: Interactive bill manager
-//
-// Summary:
-//   Create a command line bills/expenses manager that runs
-//   interactively. This mini project brings together many of
-//   the concepts learn thus far into a single application.
-//
-//   The user stories/requirements are split into stages.
-//   Fully implement each stage as a complete working program
-//   before making changes for the next stage. Leverage the
-//   compiler by using `cargo check --bin p1` when changing
-//   between stages to help identify adjustments that need
-//   to be made.
-//
-// User stories:
-// * Stage 1:
-//   - I want to add bills, including the name and amount owed.
-//   - I want to view existing bills.
-// * Stage 2:
-//   - I want to remove bills.
-// * Stage 3:
-//   - I want to edit existing bills.
-//   - I want to go back if I change my mind.
-//
-// Tips:
-// * Use the loop keyword to create an interactive menu.
-// * Each menu choice should be it's own function, so you can work on the
-//   the functionality for that menu in isolation.
-// * A vector is the easiest way to store the bills at stage 1, but a
-//   hashmap will be easier to work with at stages 2 and 3.
 use std::io;
+
+#[derive(Debug, Clone)]
+pub struct Bill {
+    name: String,
+    amount: f64,
+}
+
+pub struct Bills {
+    inner: Vec<Bill>,
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self { inner: vec![] }
+    }
+    fn add(&mut self, bill: Bill) {
+        self.inner.push(bill);
+    }
+
+    fn get_all(&self) -> Vec<&Bill> {
+        self.inner.iter().collect()
+    }
+}
 
 fn get_input() -> Option<String> {
     let mut buffer = String::new();
@@ -40,6 +33,50 @@ fn get_input() -> Option<String> {
         None
     } else {
         Some(input)
+    }
+}
+
+fn get_bill_amount() -> Option<f64> {
+    println!("Amount:");
+    loop {
+        let input = match get_input() {
+            Some(input) => input,
+            None => return None,
+        };
+        if &input == "" {
+            return None;
+        }
+        let parsed_input: Result<f64, _> = input.parse();
+        match parsed_input {
+            Ok(amount) => return Some(amount),
+            Err(_) => println!("please enter a number"),
+        }
+    }
+}
+
+mod menu {
+    use crate::{get_bill_amount, get_input, Bill, Bills};
+
+    pub fn add_bill(bills: &mut Bills) {
+        println!("Bill name:");
+        let name = match get_input() {
+            Some(input) => input,
+            None => return,
+        };
+        let amount = match get_bill_amount() {
+            Some(amount) => amount,
+            None => return,
+        };
+
+        let bill = Bill { name, amount };
+        bills.add(bill);
+        println!("Bill added");
+    }
+
+    pub fn view_bills(bills: &Bills) {
+        for bill in bills.get_all() {
+            println!("{:?}", bill)
+        }
     }
 }
 
@@ -69,12 +106,13 @@ impl MainMenu {
 }
 
 fn main() {
+    let mut bills = Bills::new();
     loop {
         MainMenu::show();
         let input = get_input().expect("no data entered");
         match MainMenu::from_str(input.as_str()) {
-            Some(MainMenu::AddBill) => (),
-            Some(MainMenu::ViewBill) => (),
+            Some(MainMenu::AddBill) => menu::add_bill(&mut bills),
+            Some(MainMenu::ViewBill) => menu::view_bills(&bills),
             None => return,
         }
     }
